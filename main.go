a31b71931bec5bcf5e2979bdaa30971c3ed79425
maciej palczynski <palczynski.maciej@gmail.com>
2021-08-06 12:09:05 +0200

    added comments and close a file

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ import (
 
 var fileName = flag.String("f", "gopher.json", "Specify the name of the story file")
 
+// tpl is a template of how the story page will look like.
+// The body of the page will show story and beneath it will be hyperlinked options with links to the next chapter
+// In case of no further options "THE END" will appear.
 const tpl = `
 <!DOCTYPE html>
 <html>
@@ -28,6 +31,7 @@ const tpl = `
 </html>
 `
 
+// Options and place are structures that are used for parsing structured .json file
 type Options struct{
 	Text string `json:"text"`
 	Arc string `json:"arc"`
@@ -39,12 +43,13 @@ type place struct{
 	Options []Options `json:"options"`
 }
 
+// main has only two functions responsible for handling and listening on a given port
 func main() {
 	http.HandleFunc("/", tempHandler)
 	http.ListenAndServe(":8080", nil)
-
 }
 
+// tempHandler is serving properly parsed template with provided data on the server
 func tempHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[1:]
 	t, err := template.New("webpage").Parse(tpl)
@@ -56,12 +61,14 @@ func tempHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, d[title])
 }
 
+// getData is parsing .json file and returning it in a form of a map[string]place
 func getData() map[string]place	{
 	flag.Parse()
 	f, err := os.Open(*fileName)
 	if err != nil {
 		fmt.Println("Couldn't open file")
 	}
+	defer f.Close()
 
 	jsonData, err := ioutil.ReadAll(f)
 	var data map[string]place
